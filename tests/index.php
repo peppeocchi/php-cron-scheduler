bf463133963e49d29968bb66bffc57ebef8d90cd
Giuseppe Occhipinti <peppeocchi@gmail.com>
2015-06-30 12:22:22 +0100

    Updated example

--- tests/index.php
+++ tests/index.php
@@ -1,9 +1,8 @@
 <?php require_once __DIR__ . '/../vendor/autoload.php';
 
-
 use GO\Scheduler;
 
-$scheduler = new Scheduler(__DIR__);
+$scheduler = new Scheduler();
 $scheduler->schedule(__DIR__.'/command.php', '* * * * *');
 
 $scheduler->run();
