7966f17f93a8be61db7933758cd343a2510adfc0
clivend <clivend@outlook.com>
2015-07-02 12:39:15 +0200

    prova

--- tests/index.php
+++ tests/index.php
@@ -3,7 +3,11 @@
 use GO\Scheduler;
 
 $scheduler = new Scheduler();
+
+// Schedule command.php to run every minute
 $scheduler->schedule(__DIR__.'/command.php', '* * * * *');
 
+// Schedule command.php to run every day at 08:30
+$scheduler->schedule(__DIR__.'/command.php', '30 08 * * *');
 
 $scheduler->run();
