d317e8390c8439e87123b190191783ff1f63ec6a
Giuseppe Occhipinti <peppeocchi@gmail.com>
2015-07-10 09:25:43 +0100

    Test scripts

---
+++ tests/cronjob.php
@@ -0,0 +1,3 @@
+<?php
+
+echo 'I am a cronjob output!';
--- tests/index.php
+++ tests/index.php
@@ -3,15 +3,47 @@
 use GO\Scheduler;
 
 $scheduler = new Scheduler();
-// $res = $scheduler->php(__DIR__.'/command.php',[
-//   '--foo' => 'bar',
-//   '--baz' => '',
-//   '-S' => '"Mi Awesome File"',
-// ])->at('* * * * *');
-$scheduler->php(__DIR__.'/../src/GO/Job/Job.php')->at('sa')->output('myfile')->email('myemail');
-// $raw = $scheduler->raw('php artisan clean:cache all')->at('10:00');
-// $closure = $scheduler->call(function () {
-//   return 28 + 1;
-// })->at('25 May 2016 00:00');
 
-var_dump($scheduler->run());
+// Schedule cronjob.php to run every minute
+$scheduler->php(__DIR__.'/../tests/cronjob.php')->at('* * * * *');
+
+// Schedule a raw command to tun every minute between 00 and 04 of every hour, send the output to raw.log
+$scheduler->raw('echo "I am a raw command!"')
+  ->at('00-04 * * * *')
+  ->output(__DIR__.'/../tests/raw.log');
+
+// Schedule a command and send output to cronjob.log - append to the existing file
+$scheduler->php(__DIR__.'/../tests/cronjob.php')
+  ->at('* * * * *')
+  ->output(__DIR__.'/../tests/cronjob.log', true);
+
+// Send output to multiple files
+$scheduler->php(__DIR__.'/../tests/cronjob.php')
+  ->at('* * * * *')
+  ->output([
+    __DIR__.'/../tests/cronjob.log',
+    __DIR__.'/../tests/my_other.log',
+  ], true);
+
+// Send output to file and to email
+$scheduler->php(__DIR__.'/../tests/cronjob.php')
+  ->at('* * * * *')
+  ->output(__DIR__.'/../tests/cronjob.log', true)
+  ->email('my@cool.email');
+
+// Send output to multiple emails
+$scheduler->php(__DIR__.'/../tests/cronjob.php')
+  ->at('* * * * *')
+  ->email(['my@cool.email', 'my@othercool.email']);
+
+// Pretty scheduling - run every day at 10:30
+$scheduler->php(__DIR__.'/../tests/cronjob.php')
+  ->every()
+  ->day('10:30');
+
+// Pretty scheduling - run every 25th of month at 00:13
+$scheduler->php(__DIR__.'/../tests/cronjob.php')
+  ->every()
+  ->month('25 00:13');
+
+$scheduler->run();
