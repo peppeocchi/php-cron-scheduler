24dd55bbff322e6c4bf2b53aaa977939b9aea81d
Andrew Male <mamale@outlook.com>
2022-01-05 23:20:26 -0500

    Adding guard for Windows bin paths with spaces

--- src/GO/Scheduler.php
+++ src/GO/Scheduler.php
@@ -126,6 +126,10 @@ class Scheduler
         $bin = $bin !== null && is_string($bin) && file_exists($bin) ?
             $bin : (PHP_BINARY === '' ? '/usr/bin/php' : PHP_BINARY);
 
+				if (PHP_OS_FAMILY == 'Windows' && stripos($bin, ' ') !== false) {
+					$bin = '"{$bin}"';
+				}
+
         $job = new Job($bin . ' ' . $script, $args, $id);
 
         if (! file_exists($script)) {
